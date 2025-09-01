21bec5a9b3e03b7720d75f666fe0d25c43514934
albesroger <albescaballero@gmail.com>
2025-09-01 12:06:22 -0400

    agregar modelo task

---
+++ backend/src/models/task.ts
@@ -0,0 +1,72 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../database/connection.js";
+
+export const Task = sequelize.define("Task", {
+  //Identificación
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+  },
+  taskID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name_of_item_writer: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  team: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+
+  //Tema y Contenido
+  main_topic_area: {
+    type: DataTypes.STRING,
+  },
+  nature_of_content: {
+    type: DataTypes.STRING,
+  },
+  vocabulary: {
+    type: DataTypes.STRING,
+  },
+  grammar: {
+    type: DataTypes.STRING,
+  },
+
+  //Nivel y Tiempo
+  time_to_do_total_task: {
+    //in minutes
+    type: DataTypes.INTEGER,
+  },
+  task_level_estimated: {
+    type: DataTypes.INTEGER,
+  },
+
+  //Resultados Esperados
+  expected_outcomes: {
+    type: DataTypes.STRING,
+  },
+
+  //Comentarios
+  comments: {
+    type: DataTypes.STRING,
+  },
+
+  //Feedback
+  feedback_provided_by: {
+    type: DataTypes.STRING,
+  },
+  feedback_date: {
+    type: DataTypes.DATE,
+  },
+  feedback_team: {
+    type: DataTypes.STRING,
+  },
+});
+
